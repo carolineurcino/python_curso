07aa079fcaadd81d842aac9517837a97d8e200d8
carolineurcino <carolinedasilva.urcino@gmail.com>
2024-12-02 10:00:40 -0300

    Criação de variáveis e print concatenação

---
+++ Aula2/aula2.py
@@ -0,0 +1,10 @@
+print("ola mundo")
+nome_usuario = "Caroline" #tipo -> str = string
+idade_usuario = 17 #tipo -> = int
+altura = 1.55 #tipo -> = float
+maior_de_idade = False #tipo -> bool = boolean
+print(nome_usuario)
+print(idade_usuario)
+print(altura)
+print(maior_de_idade)
+print ('seja bem vindo',nome_usuario, 'sua idade é', idade_usuario, 'sua altura é', altura, 'você é maior de idade?', maior_de_idade)
